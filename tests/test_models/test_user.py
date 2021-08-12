e8f176d01337d4b855280f8cb3272814d8d4def7
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-12 01:04:44 +0000

    added tests for users

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+""" User unittests"""
+import unittest
 from models.user import User
+import pep8
 
 
-class test_User(test_basemodel):
-    """ """
+class test_User(unittest.TestCase):
+    """Test for user"""
+    def test_user(self):
+        '''  Test for User '''
+        u = User(email='Benny@mailcaliente.com', password='FichadePlatino',
+                 first_name='Benito', last_name='Camelo')
+        self.assertEqual(str, type(u.email))
+        self.assertEqual(str, type(u.password))
+        self.assertEqual(str, type(u.first_name))
+        self.assertEqual(str, type(u.last_name))
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    def test_pep8_console(self):
+        """test pep8 style"""
+        s = pep8.StyleGuide(quiet=True)
+        pep = s.check_files(['models/user.py'])
+        self.assertEqual(pep.total_errors, 0, 'Found errors.')
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
-
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
-
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
-
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+if __name__ == '__main__':
+    unittest.main()
