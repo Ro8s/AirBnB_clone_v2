e4755a53aaa1a5a7269c429cac24f121ba36d968
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-27 01:35:58 +0000

    added ex 0

---
+++ web_static/0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+sudo apt-get update -y
+sudo apt-get install nginx -y
+mkdir -p /data/web_static/releases/test/
+printf %s "xD
+" > /data/web_static/releases/test/index.html
+ln -sfn /data/web_static/current /data/web_static/releases/test/
+chown -R ubuntu:ubuntu /data/
+sed -i '/server_name _;/a \        location /hbnb_static {\n' /etc/nginx/sites-available/default
+sed -i '/hbnb_static {/a \                alias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
+sudo service nginx restart
