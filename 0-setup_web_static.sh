d49e561e2ad9fd438fe0169bd967616bce0faaae
Ro8s <rodrigo.delgado3132@gmail.com>
2021-08-27 01:55:13 +0000

    fix ex 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 printf %s "xD
 " > /data/web_static/releases/test/index.html
-ln -sfn /data/web_static/current /data/web_static/releases/test/
+ln -sfn /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i '/server_name _;/a \        location /hbnb_static {\n' /etc/nginx/sites-available/default
 sed -i '/hbnb_static {/a \                alias /data/web_static/current/;\n}' /etc/nginx/sites-available/default
