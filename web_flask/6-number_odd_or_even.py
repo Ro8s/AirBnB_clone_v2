3174cd3a8f2628caf6c8ef2ad4b8e6820cbe7ffe
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-12 23:54:29 +0000

    fix pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -41,7 +41,7 @@ def number_template(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def even_odd(n):
-    kind=""
+    kind = ""
     if (n % 2 == 0):
         kind = "even"
     else:
