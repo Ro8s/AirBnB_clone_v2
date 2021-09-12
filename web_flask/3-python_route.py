21e3dc18af33a758baa28891b6a058db987b1c56
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-12 04:50:57 +0000

    added ex 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+''' script that starts a Flask web application '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def holis():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def isC(text):
+    t = text.replace("_", " ")
+    return 'C {}'.format(t)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    if text != "":
+        t = text.replace("_", " ")
+        return 'Python {}'.format(t)
+    else:
+        return 'Python is cool'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
