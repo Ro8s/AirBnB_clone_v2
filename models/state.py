5c627ce9e66b9fdbd1479d582f92424fd3b43deb
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-13 01:22:51 +0000

    a ver si quiere

--- models/state.py
+++ models/state.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
+from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage
 from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if (getenv("HBNB_TYPE_STORAGE") == "db"):
-        cities = relationship('City', backref="state",
-                              cascade="all, delete-orphan")
 
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
-            """ cities """
-            from models import storage
-            from models.city import City
-            mycities = {}
-            for city, value in storage.all(City).items():
-                if (value.state_id == self.id):
-                        mycities.update({city: value})
-            return mycities
+            '''Cities relation with state'''
+            ret = []
+            cities_objs = storage.all(City)
+            for key in cities_objs:
+                if cities_objs[key].state_id == self.id:
+                    ret.append(cities_objs[key])
+            return ret
